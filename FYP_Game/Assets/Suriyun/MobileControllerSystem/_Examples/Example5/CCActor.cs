c0cd8128fd2dbd1afcb818ff186212a1c2843c1f
Muhammad Kamran <62402727+kamranarts075@users.noreply.github.com>
2025-01-03 00:38:35 +0500

    Update

---
+++ FYP_Game/Assets/Suriyun/MobileControllerSystem/_Examples/Example5/CCActor.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Suriyun.MCS;
+
+public class CCActor : MonoBehaviour {
+
+    public Camera cam;
+    public UniversalButton inputMove;
+    public float moveSpeed;
+
+    protected CharacterController controller;
+
+    protected virtual void Start() {
+        controller = gameObject.AddComponent<CharacterController>();
+    }
+
+    protected Vector3 desiredDirection;
+    protected virtual void Update() {
+        this.UpdateMovementAxis();
+
+        if (inputMove.isFingerDown) {
+            desiredDirection = inputMove.directionXZ.x * horizontalAxis + inputMove.directionXZ.z * verticalAxis;
+            transform.forward = desiredDirection;
+        } else {
+            desiredDirection = Vector3.zero;
+        }
+
+        controller.Move(desiredDirection * Time.deltaTime * moveSpeed);
+    }
+
+    protected Vector3 verticalAxis;
+    protected Vector3 horizontalAxis;
+    protected virtual void UpdateMovementAxis() {
+        verticalAxis = cam.transform.forward;
+        verticalAxis.y = 0;
+        verticalAxis.Normalize();
+
+        horizontalAxis = cam.transform.right;
+        horizontalAxis.y = 0;
+        horizontalAxis.Normalize();
+    }
+
+}
